05bf99ce13ecf5781734c67858e7f644d78857c8
Nicolas Quinn <nclsquinn@gmail.com>
2022-10-08 11:53:15 -0300

    Class 74 - Creating a categories list

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react"
 
 export const GifExpertApp = () => {
+
+    const [ categories, setCategories ] = useState([ 'Better Call Saul', 'Breaking Bad' ]);
+
   return (
     <>
         <h1>GifExpertApp</h1>
+        <ol>
+        {
+            categories.map( category => {
+                return <li key={ category }>{ category }</li>
+            })
+        }
+        </ol>
     </>
   )
 }
