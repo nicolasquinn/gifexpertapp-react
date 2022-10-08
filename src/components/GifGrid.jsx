91a7d088dfd7f523c2710821da1794e4d2a6c4bf
Nicolas Quinn <nclsquinn@gmail.com>
2022-10-08 19:26:49 -0300

    Class 82 - useEffect

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,14 +1,25 @@
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 // Se recibe de manera implícita el key.
 export const GifGrid = ({ category }) => {
+  
+  const [ counter, setCounter ] = useState(10);
 
-  getGifs(category);
+  const handlerAdd = () => {
+    setCounter( counter + 1 );
+  }
+
+  useEffect( () => {
+    getGifs(category);
+  }, [])
 
   return (
     <>
         <h3>{ category }</h3>
         <p>La imagen</p>
+        <p> { counter } </p>
+        <button onClick={ handlerAdd }>+1</button>
     </>
   )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,7 @@
 export const getGifs = async (category) => {
 
     const key = 'NfjyWicXWThaqg5U5vkFFoFZoh6uFAJ7'
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${ key }&q=${ category }&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${ key }&q=${ category }&limit=10`;
     const resp = await fetch(url)
     const { data } = await resp.json();
     
